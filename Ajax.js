a9a902c6e8b9b0b8e4e7fa2618f441a3b4e32914
Bassem <41355488+users-profile@users.noreply.github.com>
2021-08-23 00:30:11 +0200

    Update Ajax.js

--- Ajax.js
+++ Ajax.js
@@ -22,4 +22,12 @@ $( document ).ready(function() {
 
     });
     
+    
+    $(document).keypress(function(event){
+        var keycode = (event.keyCode ? event.keyCode : event.which);
+        if(keycode == '13'){
+            $(".btnSend").click();    
+        }
+    });
+    
 });
